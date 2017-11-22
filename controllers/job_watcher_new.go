ad744262d9be9627e3d5b0ec5c7e3b541b3483d5
xinzhiyun <xinzhiyun@xinzhiyundeMacBook-Pro.local>
2017-11-22 21:50:26 +0800

    fix webhooks bug

--- controllers/job_watcher_new.go
+++ controllers/job_watcher_new.go
@@ -207,6 +207,7 @@ func (queue *StageQueueNew) WatchJob(namespace, jobName string) *v1beta1.Job {
 				glog.Infof("%s %s,status:%v\n", method, "收到ADD事件,开始起job进行构建", dm.Status)
 				//return dm
 				//成功时并且已经完成时
+				continue
 			} else if event.Type == watch.Deleted {
 				//收到deleted事件，job可能被第三方删除
 				glog.Errorf("%s  %s status:%v\n", method, " 收到deleted事件，job可能被第三方删除", dm.Status)
