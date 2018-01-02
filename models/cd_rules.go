bdd8086592181c828bea87f93cc6db756d14d461
qinzhao <xinzhiyun@xinzhiyundeMacBook-Pro.local>
2018-01-02 22:04:07 +0800

    修复CD bug

--- models/cd_rules.go
+++ models/cd_rules.go
@@ -104,6 +104,7 @@ func (cd *CDRules) DeleteDeploymentCDRule(namespace, cluster, name string, orms 
 	return nil
 }
 
+
 func (cd *CDRules) UpdateCDRule(namespace, flow_id, rule_id string, rule CDRules, orms ...orm.Ormer) (int64, error) {
 	var o orm.Ormer
 	if len(orms) != 1 {
@@ -112,7 +113,7 @@ func (cd *CDRules) UpdateCDRule(namespace, flow_id, rule_id string, rule CDRules
 		o = orms[0]
 	}
 
-	return o.Update(&rule, "Update_time", "Binding_cluster_id", "Binding_deployment_id", "Binding_deployment_name")
+	return o.Update(&rule, "update_time", "binding_cluster_id", "binding_deployment_id", "binding_deployment_name")
 }
 
 func (cd *CDRules) ListRulesByFlowId(namespace, flow_id string, orms ...orm.Ormer) (cdRules []CDRules, total int64, err error) {
