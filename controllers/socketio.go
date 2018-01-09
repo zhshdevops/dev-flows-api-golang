b0a2d13fce9f7ecc3590af0202fa593a4c29bb1c
qinzhao <xinzhiyun@xinzhiyundeMacBook-Pro.local>
2018-01-09 23:47:12 +0800

    fix logs bug

--- controllers/socketio.go
+++ controllers/socketio.go
@@ -68,7 +68,7 @@ func GetStageBuildLogsFromK8S(buildMessage EnnFlow, conn Conn) {
 	}
 
 	glog.Infof("the podName is empty============PodName=%s\n", build.PodName)
-	podName, err := imageBuilder.GetPodName(build.Namespace, build.JobName)
+	podName, err := imageBuilder.GetPodName(build.Namespace, build.JobName, build.BuildId)
 	if err != nil || podName == "" {
 		glog.Errorf("%s 获取构建任务信息失败 get job name=[%s] pod name failed:======>%v\n", method, build.JobName, err)
 		SendLog(fmt.Sprintf(`<font color="red">[Enn Flow API Error]%s</font>`, "获取构建任务信息失败"), conn)
--- models/image_builder.go
+++ models/image_builder.go
@@ -494,9 +494,9 @@ func (builder *ImageBuilder) GetLabel(flowName, stageName string) string {
 
 }
 
-func (builder *ImageBuilder) GetPodName(namespace, jobName string) (string, error) {
+func (builder *ImageBuilder) GetPodName(namespace, jobName string, buildId ...string) (string, error) {
 	method := "GetPodName"
-	pod, err := builder.GetPod(namespace, jobName)
+	pod, err := builder.GetPod(namespace, jobName, buildId[0])
 	if err != nil {
 		glog.Errorf("%s get pod name failed:====> %v\n", method, err)
 		return "", err
